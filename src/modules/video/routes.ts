d677f71583a1076d674923882947f742adcebc8c
johnson <johnson@github.com>
2023-01-26 16:42:30 -0300

    feat: criado a camada de videos

---
+++ src/modules/video/routes.ts
@@ -0,0 +1,40 @@
+import { Request, Response, Router } from "express";
+import { prismaClient } from "../../database/client-prisma";
+
+export const videoRoute = Router();
+
+videoRoute.post("/api/v1/video", async (req: Request, res: Response) => {
+  const {  title, url, describe } = req.body;
+
+  const videoCreated = await prismaClient.video.create({
+    data: {
+      title: title,
+      url: url,
+      describe: describe,
+    },
+  });
+
+  res.status(201).json(videoCreated);
+});
+
+videoRoute.get("/api/v1/video/:id", async (req: Request, res: Response) => {
+  const { id } = req.params
+ 
+  const video = await prismaClient.video.findFirst({
+    where:{
+      id: id
+    },
+    include: {
+      comments: {
+        select: {
+          text: true,
+          createdAt: true,
+          updatedAt: true,
+
+          },
+        },
+      }
+    });
+
+  res.status(200).json(video);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { videoRoute } from "modules/video/routes";
 
 const server = express();
 
 server.use(express.json());
+server.use(videoRoute)
 
 server.listen(3000, () => {
   console.log("server running on port 3000");
