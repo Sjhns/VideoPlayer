f42c9495d89f67efac2671b825fba4e189d25d22
Sjhns <108435026+Sjhns@users.noreply.github.com>
2023-01-27 20:27:23 -0300

    feat: criado header com navegação e busca

--- next.js/src/container/header/index.tsx
+++ next.js/src/container/header/index.tsx
@@ -1,20 +1,24 @@
-export const Header = () =>{
-    return(
-        <header>
-        <div >
-            <i ></i>
-        </div>
-        <div >
-            <input type="text" placeholder="Search" />
-            <i className='bx bx-search'></i>
-        </div>
-        <div >
-            <span><i className='bx bxs-video-plus'></i> Create</span>
-            <i className='bx bx-time-five'></i>
-            <i className='bx bx-bookmark-plus'></i>
-            <i className='bx bx-dots-vertical-rounded'></i>
-            <button>Log in</button>
-        </div>
-    </header>
-    )
-}
+import * as S from "./style";
+
+export const Header = () => {
+  return (
+    <S.Container>
+      <S.ContainerLogo>
+        <i className="bx bx-menu"></i>
+      </S.ContainerLogo>
+      <S.BoxSearch>
+        <input type="text" placeholder="Search" />
+        <i className="bx bx-search"></i>
+      </S.BoxSearch>
+      <S.BoxOptions>
+        <span>
+          <i className="bx bxs-video-plus"></i> Create
+        </span>
+        <i className="bx bx-time-five"></i>
+        <i className="bx bx-bookmark-plus"></i>
+        <i className="bx bx-dots-vertical-rounded"></i>
+        <button>Log in</button>
+      </S.BoxOptions>
+    </S.Container>
+  );
+};
