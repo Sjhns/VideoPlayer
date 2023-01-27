1efef1e917f568303c694ad2dd953dd5d8ff8b10
Sjhns <108435026+Sjhns@users.noreply.github.com>
2023-01-27 20:30:35 -0300

    build: adicionando padrão de tamanho

--- next.js/src/global/global-styles.tsx
+++ next.js/src/global/global-styles.tsx
@@ -1,11 +1,11 @@
-import { createGlobalStyle } from 'styled-components'
-import { Poppins } from '@next/font/google'
+import { createGlobalStyle } from "styled-components";
+import { Poppins } from "@next/font/google";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
   style: ["normal"],
-  subsets: ["latin"]
-})
+  subsets: ["latin"],
+});
 
 const GlobalStyle = createGlobalStyle`
   *{
@@ -15,10 +15,14 @@ const GlobalStyle = createGlobalStyle`
     font-family: ${poppins.style.fontFamily};
   }
 
+  html {
+    font-size: 62.5%;
+  }
+
   body{
     background-color: #181a20;
     color: #ffffff;
   }
-`
+`;
 
-export default GlobalStyle
+export default GlobalStyle;
